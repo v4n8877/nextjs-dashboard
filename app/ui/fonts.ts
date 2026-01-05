a12d87b1f546b10f3f13919bbac9491f488b75bf
vinhngo <vinh.ngo@madison-technology.com>
2026-01-05 08:28:14 +0700

    fix err font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lora } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lora = Lora({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
